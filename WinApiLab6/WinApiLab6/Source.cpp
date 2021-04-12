5a6279ae5beee0c63d93512137bd479696c9fafd
DewhiteE <dewhiteeofficial@gmail.com>
2021-04-13 00:52:07 +0300

    update lab6

--- WinApiLab6/WinApiLab6/Source.cpp
+++ WinApiLab6/WinApiLab6/Source.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <thread>
+#include <windows.h>
 
 // Deniss Belovs 4801BD
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
+	if (argc < 2)
+	{
+		cout << "Not enough arguments\n";
+		return 0;
+	}
+
+	DWORD dwExitCode;
+	STARTUPINFO startupInfo;
+	ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
+	PROCESS_INFORMATION pi;
+	LPSTR executableName = argv[1];
+	//if (CreateProcess(NULL, ""))
 	return 0;
 }
--- WinApiLab6/WinApiLab6_Slave/Source.cpp
+++ WinApiLab6/WinApiLab6_Slave/Source.cpp
@@ -3,8 +3,42 @@
 
 using namespace std;
 
-int main(int argc, wchar_t** argv)
+int main(int argc, char** argv)
 {
-	//HANDLE hFile = CreateFile(*argv, )
+	if (argc < 2)
+	{
+		cout << "Not enough arguments\n";
+		return 0;
+	}
+
+	LPCSTR fileName = argv[1];
+	cout << "fileName = " << fileName << endl;
+	HANDLE hFile = CreateFileA(fileName, GENERIC_READ, NULL /*FILE_SHARE_READ*/, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		SetFilePointer(hFile, 0, 0, FILE_BEGIN);
+		DWORD sizeInBytes = GetFileSize(hFile, NULL);
+		for (int i = 0; i < sizeInBytes; i++)
+		{
+			BYTE currentByte;
+			DWORD numberOfWrittenBytes;
+			if (ReadFile(hFile, &currentByte, sizeof(BYTE), &numberOfWrittenBytes, NULL))
+			{
+				cout << currentByte;
+			}
+			else
+			{
+				cout << "Error while reading the file\n";
+			}
+		}
+		cout << endl;
+		CloseHandle(hFile);
+	}
+	else
+	{
+		cout << "Error while opening the file. Check spelling of a file name\n";
+	}
+
+	//system("PAUSE");
 	return 0;
 }
