e6470febdb13822d5afe7df968354131b117e303
DewhiteE <dewhiteeofficial@gmail.com>
2021-05-31 21:56:26 +0300

    add lab8

---
+++ Laba8/Laba8/Source.txt
@@ -0,0 +1,131 @@
+﻿/* 4. Создать структуру, содержащую массив int размером 500, массив double
+размером 100 и число short;
+Создать массив структуры выше размером 8, заполнить массив int индексом
+структуры (в массиве структуры), массив double индексом делённым на 100, а
+число short индексом массива, который находится перед ним в виртуальной
+памяти (подробнее ниже) (-1 для первой структуры);
+Создать вспомогательный массив short размера 8;
+Записать структуры в память в рандомном порядке, записав порядок 
+(индексы) в вспомогательный массив;
+При считывании из виртуальной памяти, считывать массив структур в
+правильном (0-7) порядке, используя вспомогательный массив;
+Вывести на экран 10-ый элемент обоих массивов всех структур и всех чисел
+short.
+*/
+
+#include <iostream>
+#include <windows.h>
+
+struct Data {
+	int int_arr[500];
+	double double_arr[100];
+	short short_val;
+};
+
+using namespace std;
+
+bool arrContains(short a, short* arr, int n);
+
+int main(){
+	srand(time(0));
+
+	// 1)
+	SYSTEM_INFO system_info;
+	GetSystemInfo(&system_info);
+	cout << "dwPageSize = " << system_info.dwPageSize << endl;
+	cout << "dwAllocationGranularity = " << system_info.dwAllocationGranularity << endl;
+
+	// 2)
+	Data* a = (Data*)VirtualAlloc(NULL, system_info.dwAllocationGranularity, MEM_RESERVE, PAGE_READWRITE);
+	a = (Data*)VirtualAlloc(a, system_info.dwPageSize, MEM_COMMIT, PAGE_READWRITE);
+	if (!a){
+		cout << "Virtual allocation failed" << endl;
+		system("PAUSE");
+		return GetLastError();
+	}
+
+	// 3)
+	Data* arr = new Data[8];
+	for (int i = 0; i < 8; i++){
+		for (int j = 0; j < 500; j++){
+			arr[i].int_arr[j] = i;
+		}
+
+		for (int j = 0; j < 100; j++) {
+			arr[i].double_arr[j] = i != 0.0 ? i / 100.0 : 0.0;
+		}
+
+		arr[i].short_val = i - 1;
+	}
+
+	cout << endl << "------- Initial array ------" << endl;
+	for (int i = 0; i < 8; i++){
+		cout << "int_arr = " << arr[i].int_arr[10] << endl;
+		cout << "double_arr = " << arr[i].double_arr[10] << endl;
+		cout << "short_val = " << arr[i].short_val << endl << endl;
+	}
+
+	short* short_arr = new short[8];
+
+	// Writing data
+	cout << "short_arr (SUPPORT INDEXES) : ";
+	for (int i = 0; i < 8; i++)
+	{
+		short index = rand() % 8;
+		while (arrContains(index, short_arr, 8)){
+			index = rand() % 8;
+		}
+		short_arr[i] = index;
+		cout << short_arr[i] << " ";
+	}
+
+	cout << endl << endl << "------- Writing data ------" << endl;
+	Data* b = a;
+	for (int i = 0; i < 8; i++){
+		Data* temp = b;
+		__try {
+			*b = arr[short_arr[i]];
+			cout << "i = " << i << ", Written Data at address = " << b << endl;
+			cout << "short_arr[" << i << "] (SUPPORT INDEX) = " << short_arr[i] << endl;
+			b++;
+			cout << "Next address = " << b << endl << endl;
+		} __except (EXCEPTION_EXECUTE_HANDLER) {
+			cout << "EXCEPTION, i = " << i << endl;
+			b = (Data*)VirtualAlloc(b, system_info.dwPageSize, MEM_COMMIT, PAGE_READWRITE);
+			cout << "Created new page at " << b << " address" << endl;
+			i--;
+			b = temp;
+		}
+	}
+
+	// Reading data
+	cout << endl << "------- Reading data ------" << endl;
+	Data* c = a;
+	for (int i = 0; i < 8; i++){
+		cout << "short_arr[" << i << "] (SUPPORT INDEX) = " << short_arr[i] << endl;
+		Data* data = (Data*)((char*)c + short_arr[i] * sizeof(Data));
+		cout << "data pointer = " << data << endl;
+		cout << "int_arr = " << data->int_arr[10] << endl;
+		cout << "double_arr = " << data->double_arr[10] << endl;
+		cout << "short_val = " << data->short_val << endl << endl;
+	}
+
+	// Free memory
+	if (!VirtualFree(a, 0, MEM_RELEASE))
+	{
+		cout << "Memory release failed." << endl;
+		system("pause");
+		return GetLastError();
+	}
+	system("pause");
+	return 0;
+}
+
+bool arrContains(short a, short* arr, int n)
+{
+	for(int i = 0; i < n; i++){
+		if (arr[i] == a)
+			return true;
+	}
+	return false;
+}
---
+++ WinApiLab8/WinApiLab8/Source.cpp
@@ -0,0 +1,213 @@
+﻿// 2. Создать массив char, содержащий все заглавные буквы латинского алфавита (A - Z);
+// Создать массив long long случайного размера от 500 до 1000, где нулевой
+// элемент будет заполнен максимальным числом этого типа каждый
+// последующий элемент массива будет на 333 меньше предыдущего;
+// Записать чередуя массивы(char[0], long long[0], char[1], long long[1]…), пока не
+// кончится массив long long, а массив char начинать с нулевого элемента при
+// достижении конца массива;
+// Вывести на экран массив char(изначального размера) и последние 5
+// элементов массива long long.
+
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// Deniss Belovs 4801BD
+
+using namespace std;
+
+pair<PCHAR, DWORD> GetCharArray();
+pair<PLONGLONG, DWORD> GetLongLongArray();
+VOID WriteToPage(pair<PCHAR, DWORD>& charArray, pair<PLONGLONG, DWORD>& longLongArray, const LPVOID& pagePtr, SYSTEM_INFO systemInfo, vector<int>& outExceptionIndices, LPVOID& outIterationPtr);
+VOID ReadFromPage(pair<PCHAR, DWORD>& charArray, pair<PLONGLONG, DWORD>& longLongArray, const LPVOID& pagePtr, const vector<int>& inExceptionIndices, const LPVOID& inIterationPtr);
+int ReleaseMemory(LPVOID beginAddress);
+
+int main()
+{
+	srand(time(NULL));
+
+	// 1.
+	SYSTEM_INFO systemInfo;
+	GetSystemInfo(&systemInfo);
+	DWORD pageCount = systemInfo.dwAllocationGranularity / systemInfo.dwPageSize;
+	cout << "Page size: " << systemInfo.dwPageSize
+		<< "\nAllocation granularity: " << systemInfo.dwAllocationGranularity 
+		<< "\nPage count: " << pageCount
+		<< endl;
+
+	// 2. Reserve memory address region of minimal allowed size for one page.
+	LPVOID beginAddress = VirtualAlloc(
+		NULL,
+		/*USN_PAGE_SIZE*/systemInfo.dwAllocationGranularity,
+		MEM_RESERVE,
+		PAGE_READWRITE
+	);
+
+	// Commiting one page
+	beginAddress = VirtualAlloc(beginAddress, systemInfo.dwPageSize, MEM_COMMIT, PAGE_READWRITE);
+
+	if (!beginAddress)
+	{
+		cout << "Virtual allocation failed.\n";
+		system("pause");
+		return GetLastError();
+	}
+
+	// Get variant data
+	pair<PCHAR, DWORD> lpCharArray = GetCharArray();
+	pair<PLONGLONG, DWORD> lpDataArray = GetLongLongArray();
+
+	// 3. Write variant data at the beginning of a reserved page
+	cout << "Virtual memory address: " << beginAddress << endl;
+	vector<int> exceptionIndices;
+	LPVOID iterationPtr;
+	WriteToPage(lpCharArray, lpDataArray, beginAddress, systemInfo, exceptionIndices, iterationPtr);
+	ReadFromPage(lpCharArray, lpDataArray, beginAddress, exceptionIndices, iterationPtr);
+
+	return ReleaseMemory(beginAddress);
+}
+
+pair<PCHAR, DWORD> GetCharArray()
+{
+	cout << "\nCreating char array:\n";
+	const DWORD dwCharArraySize = 'Z' - 'A' + 1;
+	cout << "dwCharArraySize: " << dwCharArraySize << endl;
+	PCHAR lpCharArray = new CHAR[dwCharArraySize + sizeof(CHAR)];
+	for (CHAR i = 0; i < dwCharArraySize; i++)
+	{
+		lpCharArray[i] = i + 'A';
+		cout << "i: " << (int)i << " " << lpCharArray[i] << endl;
+	}
+	lpCharArray[dwCharArraySize] = '\0';
+	cout << "lpCharArray = " << lpCharArray << ", address = " << (LPVOID)lpCharArray << ", strlen(lpCharArray) = " << strlen(lpCharArray) << endl;
+	return make_pair(lpCharArray, dwCharArraySize);
+}
+
+pair<PLONGLONG, DWORD> GetLongLongArray()
+{
+	cout << "\nCreating long long array:\n";
+	const DWORD dwLongLongArraySize = rand() % 1000 + 500;
+	cout << "dwLongLongArraySize: " << dwLongLongArraySize << endl;
+	PLONGLONG lpLongLongArray = new LONGLONG[dwLongLongArraySize];
+	for (int i = 0; i < dwLongLongArraySize; i++)
+		lpLongLongArray[i] = MAXLONGLONG - 333 * static_cast<long long>(i);
+
+	cout << "Last five long long elements:\n";
+	for (int i = 0; i < 5; i++)
+		cout << "i: " << dwLongLongArraySize - 1 - i << ", " << lpLongLongArray[dwLongLongArraySize - 1 - i] << endl;
+
+	cout << endl;
+	return make_pair(lpLongLongArray, dwLongLongArraySize);
+}
+
+VOID WriteToPage(pair<PCHAR, DWORD>& charArray, pair<PLONGLONG, DWORD>& longLongArray, const LPVOID& pagePtr, SYSTEM_INFO systemInfo, vector<int>& outExceptionIndices, LPVOID& outIterationPtr)
+{
+	DWORD commitedPageCount = 1;
+	LPVOID iterationPtr = pagePtr;
+	cout << "Press any button to start writing to virtual memory!\n";
+	system("pause");
+	cout << "--- WRITING TO VIRTUAL MEMORY:\n\nINITIAL DATA: "
+		 << "pagePtr: " << pagePtr
+		 << ", iterationPtr: " << iterationPtr
+		 << ", char array: " << charArray.first
+		 << endl;
+
+	for (int i = 0; i < longLongArray.second; i++)
+	{
+		__try
+		{
+			PCHAR chPagePtr = (PCHAR)iterationPtr;
+			cout << "\ni: " << i;
+			cout << ", Char index: " << i % charArray.second;
+			CHAR currentChar = charArray.first[i % charArray.second];
+			cout << ", Current char: " << currentChar;
+			*chPagePtr = (CHAR)*(charArray.first + (i % charArray.second));
+			cout << "\n(WROTE " << currentChar << " AT LOCATION " << (LPVOID)chPagePtr << ") ";
+			chPagePtr++;
+			cout << "Current pointer location: " << (LPVOID)chPagePtr << endl;
+			
+			PLONGLONG llPagePtr = (PLONGLONG)chPagePtr;
+			*llPagePtr = *(longLongArray.first + i);
+			cout << "(WROTE " << *llPagePtr << " AT LOCATION " << llPagePtr << ") ";
+			llPagePtr++;
+			cout << "Current pointer location: " << llPagePtr << " (shifted by " << (llPagePtr - (LPVOID)chPagePtr) * 8 << " bytes)" << endl;
+			iterationPtr = llPagePtr;
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER)
+		{
+			cout << "\n------------------------ EXCEPTION OCCURED --------------------------\n";
+			cout << "iterationPtr = " << iterationPtr << ", i = " << i << endl;
+			PBYTE bytePtr = (PBYTE)pagePtr + systemInfo.dwPageSize * commitedPageCount++;
+
+			bytePtr = (PBYTE)VirtualAlloc(
+				bytePtr,
+				systemInfo.dwPageSize,
+				MEM_COMMIT,
+				PAGE_READWRITE
+			);
+			cout << "New virtual page (of size " << systemInfo.dwPageSize << ") added, commitedPageCount: " << commitedPageCount << ", page address: " << (LPVOID)bytePtr << endl;
+			outExceptionIndices.push_back(i);
+			i--;	
+			system("pause");
+		}
+	}
+	cout << endl;
+	outIterationPtr = iterationPtr;
+}
+
+VOID ReadFromPage(pair<PCHAR, DWORD>& charArray, pair<PLONGLONG, DWORD>& longLongArray, const LPVOID& pagePtr, const vector<int>& inExceptionIndices, const LPVOID& inIterationPtr)
+{
+	cout << "\n --- READING FROM VIRTUAL MEMORY:\n";
+	cout << "Exceptions occured at: ";
+	for (int i : inExceptionIndices)
+		cout << i << " ";
+	cout << "indices.";
+
+	PCHAR charIterationPtr = (PCHAR)pagePtr;
+	cout << "\nChar array: \n";
+	DWORD currentByteShift = 0;
+	for (int i = 0; i < charArray.second; i++)
+	{
+		cout << "(at address " << (LPVOID)(charIterationPtr - currentByteShift) << ") " << *(charIterationPtr + currentByteShift) << endl;
+		currentByteShift += sizeof(CHAR) + sizeof(LONGLONG);
+	}
+
+	PBYTE llIterationPtr = (PBYTE)inIterationPtr - sizeof(LONGLONG);
+	cout << "\nFive last elements of long long array: \n";
+	currentByteShift = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "(at address " << (LPVOID)(llIterationPtr - currentByteShift) << ") " << *(PLONGLONG)(llIterationPtr - currentByteShift) << endl;
+		currentByteShift += sizeof(CHAR) + sizeof(LONGLONG);
+	}
+	cout << endl;
+
+	// Read all written data from virtual memory
+	//LPVOID iterationPtr = pagePtr;
+	//for (int i = 0; i < longLongArray.second; i++)
+	//{
+	//	if (count(inExceptionIndices.begin(), inExceptionIndices.end(), i))
+	//		cout << " ------- (EXCEPTION OCCURED HERE) ";
+	//
+	//	PCHAR chPagePtr = (PCHAR)iterationPtr;
+	//	cout << "iteration: " << i << ", (address: " << (LPVOID)chPagePtr << ") CHAR: " << *chPagePtr << ", ";
+	//
+	//	PLONGLONG llPagePtr = (PLONGLONG)++chPagePtr;
+	//	cout << "(address: " << llPagePtr << ") LONG LONG: " << *llPagePtr << endl;
+	//
+	//	iterationPtr = ++llPagePtr;
+	//}
+}
+
+int ReleaseMemory(LPVOID beginAddress)
+{
+	if (!VirtualFree(beginAddress, 0, MEM_RELEASE))
+	{
+		cout << "Memory release failed." << endl;
+		system("pause");
+		return GetLastError();
+	}
+	system("pause");
+	return 0;
+}
